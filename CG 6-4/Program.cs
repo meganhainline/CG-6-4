3ad84e2db73b13fb63c8032e9f2b6abf337c1403
Megan Hainline <meg.hainline@gmail.com>
2018-08-16 12:25:10 -0500

    change

--- CG 6-4/Program.cs
+++ CG 6-4/Program.cs
@@ -6,7 +6,16 @@ namespace CG_6_4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Please enter a date formatted MM/DD/YYYY.");  //gets first date from user
+                string userValue = Console.ReadLine();
+            Console.WriteLine("Please enter another date formatted MM/DD/YYYY.");  //gets second date from user
+                string userValue2 = Console.ReadLine();
+            DateTime value1 = DateTime.Parse(userValue);  //converts user values into date times
+            DateTime value2 = DateTime.Parse(userValue2);
+            TimeSpan time = value1.Subtract(value2);    //subtracts second datetime from first datetime
+            Console.WriteLine(time.TotalDays);     //outputs number of days between the two dates
+
+            Console.ReadLine();
         }
     }
 }
